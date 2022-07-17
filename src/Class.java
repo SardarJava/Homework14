e533608cf342cfdb21b830a66abe42e50a3d4c60
SardarJava <sardar.rmfc@gmail.com>
2022-07-17 20:30:04 +0300

    Initial commit

---
+++ src/Class.java
@@ -0,0 +1,41 @@
+public class Class {
+
+    String word;
+    int number;
+    int [] array;
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public int[] getArray() {
+        return array;
+    }
+
+    public void setArray(int[] array) {
+        for (double a : array) {
+            System.out.println(a);
+
+        }
+    }
+
+    public Class(String word, int number, int[] array) {
+        this.word = word;
+        this.number = number;
+        for (double a : array) {
+            System.out.print(a + "  ");
+        }
+    }}
+
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Class space = new Class("Orbital Class ", 7, new int[] {12, 16, 19, 29});
+        System.out.print(space.word);
+        System.out.print(space.number);
+
+    }
+}
